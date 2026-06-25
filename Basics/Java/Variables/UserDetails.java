5e13a15a75312126a45c053b6c27df8009e6db87
ManasaKothakota <manasakothakota06@gmail.com>
2026-06-25 20:43:13 +0530

    Added UserDetails program to Variables

---
+++ Basics/Java/Variables/UserDetails.java
@@ -0,0 +1,16 @@
+//Writing a program using variables and data types
+class UserDetails{
+    public static void main(String[] args){
+        String name = "Manasa";
+        int  age = 22;
+        float height = 5.2f;
+        double weight = 200.90;
+        char gender = 'F' ;
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Weight: " + weight);
+        System.out.println("Gender: " + gender);
+
+    }
+}
