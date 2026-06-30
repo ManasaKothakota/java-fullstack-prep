ca9ee03acfaea31ba041f173d18493c36a952b0b
ManasaKothakota <manasakothakota06@gmail.com>
2026-06-30 22:21:03 +0530

    Added StudentDetails program

---
+++ Basics/Java/Variables/StudentDetails.java
@@ -0,0 +1,15 @@
+class StudentDetails{
+    public static void main(String[] args){
+        System.out.println("Student Details");
+        String name = "Manasa";
+        int rollNumber = 201;
+        String branch = "Computer Science Engineering";
+        double cGPA = 7.59;
+        System.out.println("Name of the student :" +name);
+        System.out.println("Roll Number :" +rollNumber);
+        System.out.println("Branch :" +branch);
+        System.out.println("CGPA :" +cGPA);
+        
+
+    }
+}
